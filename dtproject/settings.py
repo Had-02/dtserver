0b2cc9ea5f41c182ebaa8fe4f47eddd09318e17a
Had-02 <myserious120@gmail.com>
2025-01-16 14:40:04 +0900

    update setting.py

--- dtproject/settings.py
+++ dtproject/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-&+sx$ufopgahijdfb1v2@3y!+j5t@o__44$(j%)^cks1bhihmw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "54.197.19.19",
+    '*',
+    ]
 
 
 # Application definition
